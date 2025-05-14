805659b82a217a53a0f77bac440260974d72f061
Luaneb28 <borgesluane@hotmail.com>
2025-05-14 09:47:02 -0300

    Add files via upload

---
+++ pagina.js
@@ -0,0 +1,6 @@
+let botao = document.querySelector("#botao");
+
+
+botao.addEventListener("click", e =>{
+botao.innerHTML="Seja bem-vindos! Aqui vocês encontram algumas curiosidades sobre mim!";
+});
